14ec66f4fe8bd84ac6b1fabcf0b8a72f0cb42573
ryoukawa <ryou1110.pkmn@gmail.com>
2017-07-03 18:36:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -42,3 +42,37 @@ if(filename ==='other.html'){
   document.getElementById('form').select.onchange = function(){
      location.href = document.getElementById('form').select.value;
   }
+  
+  function setCookie(c_name,value,expiredays){
+var extime = new Date().getTime();
+var cltime = new Date(extime + (60*60*24*1000*expiredays));
+var exdate = cltime.toUTCString();
+    
+var s="";
+s += c_name +"="+ escape(value);
+s += path="+ location.pathname;
+if(expiredays){
+   s += "; expirs=" +exdate+"; ";
+}else{
+    s += "; ";
+}
+    
+  document.cookie=s;
+}
+
+  function getCookie(c_name){
+    var st="";
+    var ed="";
+if(0 < document.cookie.length){
+  st=document.cookie.indexOf(c_name + "=");
+  if(st!=-1){
+    st=st+c_name.lenght+1;
+    ed=document.cookie.indexOf(";",st);
+    if(ed==-1) ed=document.cookie.length;
+    
+    return unescape(document.cookie.substring(st,ed));
+  }
+}
+return "";
+}
+  
