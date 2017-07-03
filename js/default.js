86e580c4d9809f27b15e41c39b39c4d519117f41
ryoukawa <ryou1110.pkmn@gmail.com>
2017-07-03 19:35:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@ var separate_time =function(time){
   var sec   = Math.floor((time / 1000)%60);
   var min   =  Math.floor((time / 1000)%60); Math.floor((time / 1000 /60)%60);
   var hours =  Math.floor((time / 1000 /60/60) %24);
-  var days   =  Math.floor((time / 1000 /60 /60/ 24);
+  var days   =  Math.floor(time / 1000 /60 /60/ 24);
   return[sec,min,hours,days];
 }
 
