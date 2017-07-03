d56e17b68f2ba565d1fa5de417ad99d6adbb74b0
ryoukawa <ryou1110.pkmn@gmail.com>
2017-07-03 17:35:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,20 +1,26 @@
 var separate_time =function(time){
-  var sec   = time.getSeconds();
-  var min   = time.getMinutes();
-  var hours = time.getHours();
-  var days   = time.getDay();
-  var month = time.getMonth();
-  var year  = time.getFullYear();
-  return[sec,min,hours,days,month,year];
+  var sec   = Math.floor((time / 1000)%60);
+  var min   =  Math.floor((time / 1000)%60); Math.floor((time / 1000 /60)%60);
+  var hours =  Math.floor((time / 1000 /60/60) %24);
+  var days   =  Math.floor((time / 1000 /60 /60/ 24);
+  return[sec,min,hours,days];
 }
 
+var update = function(){
 var now = new Date();
-var counter = separate_time(now);
+var target = new Date(2020,7,24,0,0,0,0);
+var diff = target.getTime() - now.getTime();
+var counter = separate_time(diff);
 document.getElementById('countdown').textContent =
-  counter[5] + '年' +
-  counter[4] + '月' +
+ '東京オリンピックまであと　'　＋
   counter[3] + '日' +
   counter[2] + '時' +
   counter[1] + '分' +
   counter[0] + '秒' ;
-
+ refresh();
+  }
+  
+  var refresh= function(){
+    setTimeout(update, 1000);
+  }
+  update();
